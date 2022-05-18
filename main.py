10633a01989605ecace77d408c5e172206e58d46
Cheolsung Park <cheolsung@hanyang.ac.kr>
2022-05-18 14:19:31 +0900

    Lecture 170 (donut)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from random import randint
+import math
+
 
 my_screen = Screen()
 my_screen.colormode(255)
@@ -8,12 +10,15 @@ ching.hideturtle()
 ching.speed(0)
 print(ching.pensize(1))
 
-number_of_circles = 100
+number_of_circles = 50
+radius = 100
 for i in range(number_of_circles):
     ching.color(randint(0,255),randint(0,255),randint(0,255))
-    angle = 365/number_of_circles
-    ching.circle(100)
-    ching.seth(angle*(i+1))
+    angle = 2*math.pi/number_of_circles
+    ching.penup()
+    ching.goto(radius*math.cos(angle*i),radius*math.sin(angle*i))
+    ching.pendown()
+    ching.circle(150)
 
 my_screen.screensize(800,800)
 my_screen.exitonclick()
